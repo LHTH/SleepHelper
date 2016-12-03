672df31b36125237c05f2ad6f8b3b41a56d3a3ed
Leonard <leonard.ha@gmail.com>
2016-12-03 17:52:52 -0500

    Added favicon

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
 <meta charset = "UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content = "width=device-width, initial-scale = 1">
-<title>Sleep</title>
+<title>Sleep Helper</title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="sleep.css"/>
+
+<link rel="icon" 
+      type="image/png" 
+      href="favicon.png">
+
 </head>
 
 <body>
