04e0f12782d89ee16f5b9df10ca3f92f09f0943e
Leonard <leonard.ha@gmail.com>
2016-12-03 06:22:46 -0800

    First push

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset = "UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content = "width=device-width, initial-scale = 1">
+<title>Sleep</title>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="sleep.css"/>
+</head>
+
+<body>
+
+<!--
+Why can't I sleep?
+
+Options:
+
+- I consumed something
+	What did you consume?
+	1. Tea
+	2. Coffee
+	3. Energy drink
+	4. Chocolate
+
+- I'm worried about something
+
+- Insomnia/mental illnesses
+
+- I slept too much last night
+	When did you last sleep? (Calculates when a suitable time to try to fall asleep is)
+
+- I have an irregular sleep schedule
+
+- I just can't fall asleep.
+
+-->
+      
+
+</body>
+
+<script src="sleep.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+</html>
+
